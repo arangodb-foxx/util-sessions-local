3a6b009737e2bc3f355081ed842dc7885569cec4
Alan Plum <me@pluma.io>
2015-07-07 14:51:20 +0200

    ES6ified.

--- storage.js
+++ storage.js
@@ -72,7 +72,7 @@ Session.fromClient = function (sid) {
       read: [sessions.collection.name()],
       write: [sessions.collection.name()]
     },
-    action: function () {
+    action() {
       try {
         session = sessions.byId(sid);
 
@@ -100,24 +100,24 @@ Session.fromClient = function (sid) {
 };
 
 _.extend(Session.prototype, {
-  forClient: function () {
+  forClient() {
     return this.get('_key');
   },
-  enforceTimeout: function () {
+  enforceTimeout() {
     if (this.hasExpired()) {
       throw new errors.SessionExpired(this.get('_key'));
     }
   },
-  hasExpired: function () {
+  hasExpired() {
     return this.getTTL() === 0;
   },
-  getTTL: function () {
+  getTTL() {
     if (!cfg.timeToLive) {
       return Infinity;
     }
     return Math.max(0, this.getExpiry() - Date.now());
   },
-  getExpiry: function () {
+  getExpiry() {
     if (!cfg.timeToLive) {
       return Infinity;
     }
@@ -127,7 +127,7 @@ _.extend(Session.prototype, {
     }
     return this.get(prop) + cfg.timeToLive;
   },
-  setUser: function (user) {
+  setUser(user) {
     if (user) {
       this.set('uid', user.get('_id'));
       this.set('userData', user.get('userData'));
@@ -137,14 +137,14 @@ _.extend(Session.prototype, {
     }
     return this;
   },
-  save: function () {
+  save() {
     const now = Date.now();
     this.set('lastAccess', now);
     this.set('lastUpdate', now);
     sessions.replace(this);
     return this;
   },
-  delete: function () {
+  delete() {
     const now = Date.now();
     const key = this.get('_key');
     this.set('lastAccess', now);
